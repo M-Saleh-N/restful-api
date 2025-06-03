b9c17036ee82260ac341d4964656d10492630d66
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-03 17:15:39 +0300

    done

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request 
+
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class TodoList(Resource):
+    def get(self):
+        return todos
+    def post(self):
+        todo_id = len(todos) + 1
+        todos[todo_id] = request.json['task']
+        return {todo_id: todos [todo_id]}, 201
+class TodoResources(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+    def put(self, todo_id):
+        todos[todo_id] = request.json['task']
+        return {todo_id: todos[todo_id]}
+    def delete(self, todo_id):
+        del todos[todo_id]
+        return '', 204
+api.add_resource(TodoList, '/todos')
+api.add_resource(TodoResources, '/todos/<int:todo_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
