ce2217ef0565fb34848abe4a709b70430549820c
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-09 17:11:30 +0300

    added user

---
+++ user.py
@@ -0,0 +1,33 @@
+from flask import Flask, request 
+
+from flask_restful import Resource, Api
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+users = {}
+
+class User(Resource):
+    def get(self):
+        return users
+    
+    def post(self):
+        user_id = len(users) + 1
+        users[user_id] = request.json['users_name']
+        users["email"] = request.json['email']
+        return {user_id: users[user_id], "email": users["email"]}, 201
+    
+    def put(self):
+        user_id = request.json['user_id']
+        users[user_id] = request.json['users_name']
+        users["email"] = request.json['email']
+        return {user_id: users[user_id], "email": users["email"]}, 200
+    
+
+api.add_resource(User, '/users')
+
+if __name__ == '__main__':
+    app.run(debug=True)
