231da81660bf06308ad3af5d8071344bb8cfb70a
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-10 16:22:07 +0300

    tested

--- user.py
+++ user.py
@@ -23,10 +23,18 @@ class User(Resource):
    
     
 class UserResource(Resource):
+     def get (self, user_id):
+         return users[user_id]
+
      def put(self, user_id):
         users[user_id] = request.json['users_name']
         users["email"] = request.json['email']
         return {user_id: users[user_id], "email": users["email"]}, 200
+     
+     def delete(self, user_id):
+        del users[user_id]
+        return '', 204
+     
 
 api.add_resource(User, '/users')
 api.add_resource(UserResource, '/users/<int:user_id>')
